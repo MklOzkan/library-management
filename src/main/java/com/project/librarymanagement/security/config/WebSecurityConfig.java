bf0503727b3ae872e8b2a6fe4a989cb3369f2a53
Mikail Ozkan <ozkan.mkl.ozkanQgmail.com>
2024-07-07 20:38:27 +0200

    security classes are done. and loadUserByUserEmail is created in method helper

---
+++ src/main/java/com/project/librarymanagement/security/config/WebSecurityConfig.java
@@ -0,0 +1,98 @@
+package com.project.librarymanagement.security.config;
+
+import com.project.librarymanagement.security.jwt.AuthEntryPointJwt;
+import com.project.librarymanagement.security.jwt.AuthTokenFilter;
+import com.project.librarymanagement.security.service.UserDetailServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    private final UserDetailServiceImpl userDetailService;
+    private final AuthEntryPointJwt authEntryPointJwt;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf ->csrf.disable())
+            .exceptionHandling(exceptionHandling->
+                    exceptionHandling.authenticationEntryPoint(authEntryPointJwt))
+            .sessionManagement(sessionManagement ->
+                    sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(authorize->
+                    authorize.requestMatchers(AUTH_WHITE_LIST).permitAll()
+                            .anyRequest().authenticated());
+
+
+//        http.headers().frameOptions().sameOrigin();
+        http.authenticationProvider(authenticationProvider());
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter() {
+        return new AuthTokenFilter();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowedOrigins("*");
+            }
+        };
+    }
+
+    private final String[] AUTH_WHITE_LIST = {
+            "/v3/api-docs/**",
+            "swagger-ui.html",
+            "/swagger-ui/**",
+            "/",
+            "index.html",
+            "/images/**",
+            "/css/**",
+            "/js/**",
+            "/contactMessages/save",
+            "/auth/login"
+    };
+
+}
---
+++ src/main/java/com/project/librarymanagement/security/jwt/JwtUtils.java
@@ -0,0 +1,62 @@
+package com.project.librarymanagement.security.jwt;
+
+import com.project.librarymanagement.security.service.UserDetailsImpl;
+import io.jsonwebtoken.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+    private static final Logger LOGGER = LoggerFactory.getLogger(JwtUtils.class);
+
+    @Value("86400000")
+    private int jwtExpirationMs;
+
+    @Value("sdfsdfsdfs")
+    private String jwtSecret;
+
+    public String generateToken(Authentication authentication) {
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        return buildTokenFromUsername(userDetails.getUsername());
+    }
+
+    private String buildTokenFromUsername(String email) {
+        return Jwts.builder()
+            .setSubject(email)
+            .setIssuedAt(new Date())
+            .setExpiration(new Date(new Date().getTime() + jwtExpirationMs))
+            .signWith(SignatureAlgorithm.HS512, jwtSecret)
+            .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        } catch (SignatureException e) {
+            LOGGER.error("Invalid JWT signature: {}", e.getMessage());
+        }catch (MalformedJwtException e) {
+            LOGGER.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            LOGGER.error("Expired JWT token: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            LOGGER.error("Unsupported JWT token: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            LOGGER.error("JWT token is null: {}", e.getMessage());
+        }
+        return false;
+    }
+
+    public String getUsernameFromToken(String token) {
+        return Jwts.parser()
+            .setSigningKey(jwtSecret)
+            .parseClaimsJws(token)
+            .getBody()
+            .getSubject();
+    }
+}
