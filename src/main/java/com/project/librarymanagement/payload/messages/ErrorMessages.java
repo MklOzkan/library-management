1084db84c5e8254a79c04eae97c9528b4fc7e537
Tamara Kelesh <tamara.haydarova@gmail.com>
2024-07-08 12:24:37 +0300

    Messages

---
+++ src/main/java/com/project/librarymanagement/payload/messages/ErrorMessages.java
@@ -0,0 +1,16 @@
+package com.project.librarymanagement.payload.messages;
+
+public class ErrorMessages {
+
+    //author
+
+    public static final String NOT_FOUND_AUTHOR_MESSAGE = "Error: Author with id %s not found";
+
+
+
+
+
+
+
+
+}
