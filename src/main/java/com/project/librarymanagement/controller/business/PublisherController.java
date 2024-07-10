f5f7d553dd33c60853e4ca67ccc12f4e3b197a45
Yunus Atayev <yunusata2007@gmail.com>
2024-07-10 14:39:38 +0300

    Changes in Publisher : controller, mapper, request, response, service.

--- src/main/java/com/project/librarymanagement/controller/business/PublisherController.java
+++ src/main/java/com/project/librarymanagement/controller/business/PublisherController.java
@@ -1,7 +1,5 @@
 package com.project.librarymanagement.controller.business;
 
-import com.project.librarymanagement.entity.business.Publisher;
-import com.project.librarymanagement.payload.request.business.PublisherRequest;
 import com.project.librarymanagement.payload.response.business.PublisherResponse;
 import com.project.librarymanagement.service.business.PublisherService;
 import lombok.RequiredArgsConstructor;
@@ -19,9 +17,8 @@ public class PublisherController {
     private final PublisherService publisherService;
 
     @GetMapping
-    public PublisherResponse getAllPublishers() {
-        List<Publisher> publishers = publisherService.getAllPublishers();
-        return new PublisherResponse(publishers);
+    public List<PublisherResponse> getAllPublishers() {
+        return publisherService.getAllPublishers();
     }
 
 }
--- src/main/java/com/project/librarymanagement/service/business/PublisherService.java
+++ src/main/java/com/project/librarymanagement/service/business/PublisherService.java
@@ -1,19 +1,25 @@
 package com.project.librarymanagement.service.business;
 
 import com.project.librarymanagement.entity.business.Publisher;
+import com.project.librarymanagement.payload.mapper.PublisherMapper;
+import com.project.librarymanagement.payload.response.business.PublisherResponse;
 import com.project.librarymanagement.repository.business.PublisherRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class PublisherService {
 
+    private final PublisherMapper publisherMapper;
+
     private final PublisherRepository publisherRepository;
 
-    public List<Publisher> getAllPublishers() {
-        return publisherRepository.findAll();
+    public List<PublisherResponse> getAllPublishers() {
+        return publisherRepository.findAll()
+                .stream().map(publisherMapper :: mapPublisherToPublisherResponse).collect(Collectors.toList());
     }
 }
