709b9bc588a53043f04e5da1fc7ebb9c66f8bd9d
Mikail Ozkan <ozkan.mkl.ozkanQgmail.com>
2024-07-14 19:58:12 +0200

    ErrorMessages conflict handled

--- src/main/java/com/project/librarymanagement/payload/messages/ErrorMessages.java
+++ src/main/java/com/project/librarymanagement/payload/messages/ErrorMessages.java
@@ -10,6 +10,9 @@ public class ErrorMessages {
 
     public static final String NOT_FOUND_PUBLISHER_MESSAGE = "Error: Publisher with id %s not found";
 
+    //User
+    public static final String NOT_FOUND_USER_MESSAGE = "Error: User with id %s not found";
+
     //Loan
     public static final String NOT_FOUND_LOAN_MESSAGE = "Error: Loan with id %s not found";
     public static final String NOT_FOUND_LOAN_BY_USER_MESSAGE = "Error: Loan with user id %s not found";
