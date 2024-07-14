cc8fa80c031cfd5781b572a99fe811edbe4b1377
Mikail Ozkan <ozkan.mkl.ozkanQgmail.com>
2024-07-14 19:53:07 +0200

    related classes with loan are created and createLoan method is finished

--- src/main/java/com/project/librarymanagement/controller/business/LoanController.java
+++ src/main/java/com/project/librarymanagement/controller/business/LoanController.java
@@ -1,7 +1,14 @@
 package com.project.librarymanagement.controller.business;
 
+import com.project.librarymanagement.payload.request.business.LoanRequest;
+import com.project.librarymanagement.payload.response.business.LoanResponse;
+import com.project.librarymanagement.payload.response.business.ResponseMessage;
 import com.project.librarymanagement.service.business.LoanService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoanController {
 
     private final LoanService loanService;
+
+    @PreAuthorize("hasAnyAuthority('Admin','Employee')")
+    @PostMapping("/save")
+    public ResponseMessage<LoanResponse> createLoan(@RequestBody @Valid LoanRequest loanRequest){
+        return loanService.createLoan(loanRequest);
+    }
 }
--- src/main/java/com/project/librarymanagement/payload/messages/ErrorMessages.java
+++ src/main/java/com/project/librarymanagement/payload/messages/ErrorMessages.java
@@ -9,4 +9,10 @@ public class ErrorMessages {
     public static final String NOT_FOUND_AUTHOR_MESSAGE = "Error: Author with id %s not found";
 
     public static final String NOT_FOUND_PUBLISHER_MESSAGE = "Error: Publisher with id %s not found";
+
+    //Loan
+    public static final String NOT_FOUND_LOAN_MESSAGE = "Error: Loan with id %s not found";
+    public static final String NOT_FOUND_LOAN_BY_USER_MESSAGE = "Error: Loan with user id %s not found";
+    public static final String NOT_FOUND_LOAN_BY_BOOK_MESSAGE = "Error: Loan with book id %s not found";
+    public static final String CANNOT_HAVE_MORE_BOOKS = "Error: User with id %s cannot have more than %s books";
 }
