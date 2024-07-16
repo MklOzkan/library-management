8fa1244ac3caf906752e341f46e0a25f5f68a9bd
ctuka <tkoseli@hotmail.com>
2024-07-16 16:43:42 -0500

    test2

--- src/main/java/com/project/librarymanagement/payload/messages/ErrorMessages.java
+++ src/main/java/com/project/librarymanagement/payload/messages/ErrorMessages.java
@@ -1,7 +1,5 @@
 package com.project.librarymanagement.payload.messages;
 
-import java.util.Locale;
-
 public class ErrorMessages {
 
     public static final String NOT_FOUND_BOOK_MESSAGE = "Error: Book with id, %s not found";
@@ -14,6 +12,4 @@ public class ErrorMessages {
     public static final String ALREADY_CREATED_PUBLISHER_MESSAGE = "Error: Publisher with id %s is already created";
     public static final String NOT_FOUND_CATEGORY_MESSAGE = "Error: Category with id %d is not found";
     public static final String NOT_FOUND_CATEGORY_MESSAGE_BY_NAME = "Error: Category with name %s is not found";
-    public static final String CANNOT_HAVE_MORE_BOOKS = "Error: Can not have more books name %d is not found";
-    public static final String NOT_FOUND_USER_MESSAGE = "Error:User not found name %s is not found";
 }
