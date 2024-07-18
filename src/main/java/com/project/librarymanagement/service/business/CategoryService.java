2549a60ec5b9d4fb31494788ab3b87cc3247a5ed
Mikail Ozkan <ozkan.mkl.ozkanQgmail.com>
2024-07-18 16:02:55 +0200

    authorization problems are solved for Categories, and some other methods are created.

--- src/main/java/com/project/librarymanagement/service/business/CategoryService.java
+++ src/main/java/com/project/librarymanagement/service/business/CategoryService.java
@@ -1,14 +1,20 @@
 package com.project.librarymanagement.service.business;
 
 import com.project.librarymanagement.entity.business.Category;
+import com.project.librarymanagement.entity.user.User;
 import com.project.librarymanagement.payload.mapper.CategoryMapper;
+import com.project.librarymanagement.payload.messages.SuccessMessages;
 import com.project.librarymanagement.payload.request.business.CategoryRequest;
 import com.project.librarymanagement.payload.response.business.CategoryResponse;
 import com.project.librarymanagement.payload.response.business.ResponseMessage;
 import com.project.librarymanagement.repository.business.CategoryRepository;
 import com.project.librarymanagement.service.helper.MethodHelper;
+import com.project.librarymanagement.service.helper.PageableHelper;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -22,15 +28,20 @@ public class CategoryService {
     private final CategoryRepository categoryRepository;
     private final CategoryMapper categoryMapper;
     private final MethodHelper methodHelper;
+    private final PageableHelper pageableHelper;
 
-    public ResponseMessage<CategoryResponse> saveCategory(CategoryRequest categoryRequest) {
+    public ResponseMessage<CategoryResponse> saveCategory(CategoryRequest categoryRequest, HttpServletRequest httpServletRequest) {
+        String email = (String) httpServletRequest.getAttribute("email");
+        User authenticatedUser = methodHelper.loadUserByEmail(email);
+        //check if user is an Admin
+        methodHelper.isRoleAdmin(authenticatedUser);
         methodHelper.isCategoryExistByName(categoryRequest.getName());
         Category category = categoryMapper.mapCategoryRequestToCategory(categoryRequest);
         category.setSequence(returnMaxSequence() + 1);
         category.setBuiltIn(true);
         Category savedCategory = categoryRepository.save(category);
         return ResponseMessage.<CategoryResponse>builder()
-                .message("Category created successfully")
+                .message(SuccessMessages.CATEGORY_SAVE)
                 .returnBody(categoryMapper.mapCategoryToCategoryResponse(savedCategory))
                 .httpStatus(HttpStatus.CREATED)
                 .build();
@@ -49,4 +60,48 @@ public class CategoryService {
         }
     }
 
+    public Page<CategoryResponse> findCategoriesByPage(int page, int size, String sort, String type) {
+        Pageable pageable = pageableHelper.getPageableWithProperties(page, size, sort, type);
+        return categoryRepository.findAll(pageable)
+                .map(categoryMapper::mapCategoryToCategoryResponse);
+    }
+
+    public ResponseMessage<CategoryResponse> findCategoryById(Long id) {
+        Category category = methodHelper.getCategoryById(id);
+        return ResponseMessage.<CategoryResponse>builder()
+                .returnBody(categoryMapper.mapCategoryToCategoryResponse(category))
+                .httpStatus(HttpStatus.OK)
+                .build();
+    }
+
+    public ResponseMessage<CategoryResponse> updateCategory(Long id, CategoryRequest categoryRequest, HttpServletRequest httpServletRequest) {
+        String email = (String) httpServletRequest.getAttribute("email");
+        User authenticatedUser = methodHelper.loadUserByEmail(email);
+        //check if user is an Admin
+        methodHelper.isRoleAdmin(authenticatedUser);
+        Category category = methodHelper.getCategoryById(id);
+        if (!category.getName().equals(categoryRequest.getName())) {
+            methodHelper.isCategoryExistByName(categoryRequest.getName());
+        }
+        category.setName(categoryRequest.getName());
+        Category updatedCategory = categoryRepository.save(category);
+        return ResponseMessage.<CategoryResponse>builder()
+                .message(SuccessMessages.CATEGORY_UPDATE)
+                .returnBody(categoryMapper.mapCategoryToCategoryResponse(updatedCategory))
+                .build();
+    }
+
+    public ResponseMessage<CategoryResponse> deleteCategoryById(Long id, HttpServletRequest httpServletRequest) {
+        String email = (String) httpServletRequest.getAttribute("email");
+        User authenticatedUser = methodHelper.loadUserByEmail(email);
+        //check if user is an Admin
+        methodHelper.isRoleAdmin(authenticatedUser);
+        Category category = methodHelper.getCategoryById(id);
+        categoryRepository.deleteById(id);
+        return ResponseMessage.<CategoryResponse>builder()
+                .message(SuccessMessages.CATEGORY_DELETE)
+                .returnBody(categoryMapper.mapCategoryToCategoryResponse(category))
+                .httpStatus(HttpStatus.OK)
+                .build();
+    }
 }
